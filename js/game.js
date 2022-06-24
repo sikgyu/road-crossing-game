e249e2f3044d87fbdbd11dc8c3dd461bef0a3ad7
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-24 09:54:14 -0700

    🔨 Scale Sprite

--- js/game.js
+++ js/game.js
@@ -6,21 +6,34 @@ gameScene.preload = function(){
   // load images  
   this.load.image('background', 'assets/background.png');
   this.load.image('player', 'assets/player.png');
+  this.load.image('dragon', 'assets/dragon.png');
 };
 
 //  create the sprites and display them on the screen after the preload ends
 gameScene.create = function() {
-    // create bg sprite
-    let bg = this.add.sprite(0, 0, 'background');
+  // create bg sprite
+  let bg = this.add.sprite(0, 0, 'background');
 
-    // place sprite in the center
-    let gameW = this.sys.game.config.width;
-    let gameH = this.sys.game.config.height;
+  // place sprite in the center
+  let gameW = this.sys.game.config.width;
+  let gameH = this.sys.game.config.height;
 
-    bg.setPosition(gameW/2, gameH/2)
+  bg.setPosition(gameW/2, gameH/2)
 
+  let player = this.add.sprite(gameW/10, gameH/2, 'player');
+  // player.depth = 1;
+  player.setScale(0.5); // reducting the width and height by 50%
 
-
+  // create an enemy
+  let dragon = this.add.sprite(250, 180, 'dragon');
+  let dragon2 = this.add.sprite(450, 180, 'dragon');
+  
+  // flip and scale
+  dragon.flipX = true;
+  dragon2.flipX = true;
+  dragon.setScale(0.5);
+  dragon2.setScale(0.5);
+  
 };
 
 // set the configuration of the game
