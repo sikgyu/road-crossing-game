a1da8f5d5106745a6ccdf91186544e50d1d8de26
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-24 09:10:51 -0700

    :hammer: Create preload and place sprite

--- js/game.js
+++ js/game.js
@@ -1,6 +1,27 @@
 // create a new scene
-
 let gameScene = new Phaser.Scene('Game');
+
+// all asset will be pre-loaded so that all of the files will be loaded to memory and used without any delay
+gameScene.preload = function(){
+  // load images  
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+};
+
+//  create the sprites and display them on the screen after the preload ends
+gameScene.create = function() {
+    // create bg sprite
+    let bg = this.add.sprite(0, 0, 'background');
+
+    // place sprite in the center
+    let gameW = this.sys.game.config.width;
+    let gameH = this.sys.game.config.height;
+
+    bg.setPosition(gameW/2, gameH/2)
+
+
+
+};
 
 // set the configuration of the game
 let config = {
