c7f30dd27f07381330b5c89f2803cb0ed65d49d9
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-06-24 10:29:31 -0700

    :hammer: Create a treasure

--- js/game.js
+++ js/game.js
@@ -1,12 +1,19 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
 
+// initiate scene parameters
+gameScene.init = function() {
+  // player speed
+  this.playerSpeed = 1;
+};
+
 // all asset will be pre-loaded so that all of the files will be loaded to memory and used without any delay
 gameScene.preload = function(){
   // load images  
   this.load.image('background', 'assets/background.png');
   this.load.image('player', 'assets/player.png');
   this.load.image('dragon', 'assets/dragon.png');
+  this.load.image('treasure', 'assets/treasure.png');
 };
 
 //  create the sprites and display them on the screen after the preload ends
@@ -20,21 +27,32 @@ gameScene.create = function() {
 
   bg.setPosition(gameW/2, gameH/2)
 
-  let player = this.add.sprite(gameW/10, gameH/2, 'player');
-  // player.depth = 1;
-  player.setScale(0.5); // reducting the width and height by 50%
+  this.player = this.add.sprite(gameW/10, gameH/2, 'player').setScale(0.5);
+  this.dragon = this.add.sprite(250, 180, 'dragon').setScale(0.5);
+  this.dragon2 = this.add.sprite(450, 180, 'dragon').setScale(0.5);
+  this.treasure = this.add.sprite(this.sys.game.config.width - 80, gameH/2, 'treasure').setScale(0.5);
+  // flip
+  this.dragon.flipX = true;
+  this.dragon2.flipX = true;
 
-  // create an enemy
-  let dragon = this.add.sprite(250, 180, 'dragon');
-  let dragon2 = this.add.sprite(450, 180, 'dragon');
-  
-  // flip and scale
-  dragon.flipX = true;
-  dragon2.flipX = true;
-  dragon.setScale(0.5);
-  dragon2.setScale(0.5);
-  
 };
+
+// set 60 fps
+gameScene.update = function () {
+  // player walks
+  if(this.input.activePointer.isDown) {
+    this.player.x += this.playerSpeed;
+  }
+
+  // treasure overlap check
+  let playerRect = this.player.getBounds();
+  let treasureRect = this.treasure.getBounds();
+  
+  if(Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)){
+    this.scene.restart();
+  };
+
+}
 
 // set the configuration of the game
 let config = {
